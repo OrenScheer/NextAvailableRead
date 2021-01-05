5fbb6fe76d75bfcda370ebe788da8e19ca7e5f95
Oren Scheer <oren.rs723@gmail.com>
2021-01-05 10:48:27 -0500

    Implement shelf finding

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,14 @@
-from app import app
+#from app import app
 from flask import render_template, request, session, redirect
 import os
 import xml.etree.ElementTree as ET
 import xml.dom.minidom
-from rauth.service import OAuth1Service, OAuth1Session
-from app.forms import BookQueryForm
+#from app.forms import BookQueryForm
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import quote
-from app.Book import Book
-
-def get_shelves(goodreads_session, search):
-    r = goodreads_session.get("https://www.goodreads.com/shelf/list.xml", params=search)
-    root = ET.fromstring(r.content)
-    shelves = root.find("shelves")
-    ans = []
-    for shelf in shelves:
-        ans.append(shelf.find("name").text)
-    return ans
-
-def get_titles(goodreads_session, search):
-   return
+import re
+#from app.Book import Book
 
 def available(book):
     # Important classes: 
@@ -42,24 +30,61 @@ def available(book):
             return True
     return False
 
-@app.route('/', methods=["GET", "POST"])
-@app.route('/index', methods=["GET", "POST"])
+def get_shelves(user_id):
+    # Returns list of a user's Goodreads shelves
+    URL = "https://www.goodreads.com/review/list/" + str(user_id)
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    shelves = soup.find_all(class_='userShelf')
+    pattern = re.compile("(\s*)(.*)\(")
+    shelves = [pattern.match(shelf.text).group(2).strip() for shelf in shelves]
+    return shelves
+
+def get_titles(user_id, shelf):
+    titles = []
+    return titles
+
+#@app.route('/', methods=["GET", "POST"])
+#@app.route('/index', methods=["GET", "POST"])
 def index():
-    form = BookQueryForm()
-    form.shelf.choices = get_shelves(goodreads_session, search={"id": "64346486"})
-    if form.validate_on_submit():
-        #print(r.content)
-        search = {"id": "64346486", "shelf": form.shelf.data, "page": "1", "per_page": "200", "sort":"random"}
-        titles = get_titles(goodreads_session, search)
-        count = 0
-        i = 0
-        available_books = []
-        print("got titles")
-        while i < len(titles) and count < int(form.number_of_books.data):
-            if available(titles[i]):
-                available_books.append(titles[i])
-                count += 1
-                print("one available", i, count)
-                i += 1
-        return render_template("results.html", books=available_books)
-    return render_template("index.html", authorized=True, form=form)
+    # = BookQueryForm()
+    #form.shelf.choices = get_shelves(goodreads_session, search={"id": "64346486"})
+
+    #if form.validate_on_submit():
+    #    #print(r.content)
+    #    count = 0
+    #    i = 0
+    #    available_books = []
+    #    print("got titles")
+    #    while i < len(titles) and count < int(form.number_of_books.data):
+    #        if available(titles[i]):
+    #            available_books.append(titles[i])
+    #            count += 1
+    #            print("one available", i, count)
+    #        i += 1
+    #    return render_template("results.html", books=available_books)
+    #return render_template("index.html", authorized=True, form=form)
+    user_id = input("Please enter your Goodreads id: ")
+    shelves = get_shelves(user_id)
+    shelves = [shelf.lower() for shelf in shelves]
+    print("Please enter one of the following options:")
+    for shelf in shelves:
+        print(shelf)
+    shelf_choice = input("Enter your selection here: ").lower()
+    if shelf_choice not in shelves:
+        print("This is not one of your shelves. Bye.")
+        return
+    titles = get_shelves(user_id, shelf_choice)
+    i = 0
+    find_num = 5
+    count = 0
+    available_books = []
+    while i < len(titles) and count < find_num:
+        if available(titles[i]):
+            available_books.append(titles[i])
+            count += 1
+            print(titles[i].title)
+        i += 1
+
+index()
+    
