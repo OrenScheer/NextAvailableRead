9761f21ddd8d2dc8b1e2a4beea52894c50803b41
Oren Scheer <oren.rs723@gmail.com>
2021-01-10 19:08:12 -0500

    Implement web scraping for Goodreads side

--- app/config.py
+++ app/config.py
@@ -1,5 +1,3 @@
 from os import environ
 
 SECRET_KEY = environ.get("SECRET_KEY")
-API_KEY = environ.get("API_KEY")
-API_SECRET = environ.get("API_SECRET")
--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,9 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import quote
 import re
-#from app.Book import Book
+import math
+import random
+from Book import Book
 
 def available(book):
     # Important classes: 
@@ -36,33 +38,49 @@ def get_shelves(user_id):
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
     shelves = soup.find_all(class_='userShelf')
+    pattern = re.compile("(.*)(\()(\d*)")
     res = {}
     for shelf in shelves:
         text = shelf.text.strip()
-        res[text[:text.find("(")].strip("\u200e").strip().lower()] = shelf.find("a")['href']
+        res[text[:text.find("(")].strip("\u200e").strip().lower()] = (shelf.find("a")['href'] + "&print=true", 
+            int(pattern.match(text).group(3))) 
     return res
 
-def get_titles(user_id, extra_url):
-    URL = "https://www.goodreads.com" + extra_url
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    #f = open("text.txt", "w")
-    #f.write(str(soup.text.encode('utf8')))
-    #f.close()
-    print(URL)
+def get_titles(user_id, extra_url, number_of_books):
+    URL = "https://www.goodreads.com" + extra_url + "&page=0"
+    number_of_pages = math.ceil(number_of_books / 20)
+    current_page = 1
     titles = []
+    while current_page <= number_of_pages:
+        URL = URL.replace("&page="+str(current_page-1), "&page="+str(current_page))
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        books = soup.find_all(class_="bookalike review")
+        for book in books:
+            title = book.find(class_="field title").find(class_="value").text.strip()
+            if ((index := title.find("(")) != -1):
+                title = title[:index].strip()
+            author = " ".join(book.find(class_ = "field author").find("a").text.split(",")[::-1]).strip()
+            page_count = page_count if (page_count := (book.find(class_="field num_pages").find(class_="value").text.replace("pp", "").replace(",", "").strip())).isdigit() else 0
+            rating = float(book.find(class_="field avg_rating").find(class_="value").text.strip())
+            goodreads_link = "goodreads.com" + book.find("a")["href"]
+            image_link = book.find("img")["src"].replace("_SX50_", "").replace("_SY75_", "").replace("..", ".")
+            titles.append(Book(title, author, page_count, rating, goodreads_link, image_link))
+        current_page += 1
     return titles
 
 #@app.route('/', methods=["GET", "POST"])
 #@app.route('/index', methods=["GET", "POST"])
 def index():
-    # = BookQueryForm()
-    #form.shelf.choices = get_shelves(goodreads_session, search={"id": "64346486"})
+    # form = BookQueryForm()
+    # shelves = get_shelves("64346486")
+    # form.shelf.choices = shelves.keys()
 
-    #if form.validate_on_submit():
+    # if form.validate_on_submit():
     #    #print(r.content)
     #    count = 0
     #    i = 0
+    #    titles = get_titles("64346486", "/review/list/64346486-s-rrenn-gwyire?shelf=to-read&print=true", 463)
     #    available_books = []
     #    print("got titles")
     #    while i < len(titles) and count < int(form.number_of_books.data):
@@ -72,17 +90,19 @@ def index():
     #            print("one available", i, count)
     #        i += 1
     #    return render_template("results.html", books=available_books)
-    #return render_template("index.html", authorized=True, form=form)
-    user_id = input("Please enter your Goodreads id: ")
-    shelves = get_shelves(user_id)
-    print("Please enter one of the following options:")
-    for shelf in shelves.keys():
-        print(shelf)
-    shelf_choice = input("Enter your selection here: ").lower()
-    if shelf_choice not in shelves:
-        print("This is not one of your shelves. Bye.")
-        return
-    titles = get_titles("64346486", shelves[shelf_choice])
+    # return render_template("index.html", form=form)
+    # user_id = input("Please enter your Goodreads id: ")
+    # shelves = get_shelves(user_id)
+    # print("Please enter one of the following options:")
+    # for shelf in shelves.keys():
+    #    print(shelf)
+    # shelf_choice = input("Enter your selection here: ").lower()
+    # if shelf_choice not in shelves:
+    #    print("This is not one of your shelves. Bye.")
+    #    return
+    titles = get_titles("64346486", "/review/list/64346486-s-rrenn-gwyire?shelf=to-read&print=true", 463)
+    # titles = get_titles(user_id, shelves[shelf_choice][0], shelves[shelf_choice][1])
+    random.shuffle(titles)
     i = 0
     find_num = 5
     count = 0
