2ca945d24c7e227a396bbcd0ab20883ccc54e074
Oren Scheer <oren.rs723@gmail.com>
2021-01-10 19:30:57 -0500

    Make Flask app functional again

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,16 @@
-#from app import app
+from app import app
 from flask import render_template, request, session, redirect
 import os
 import xml.etree.ElementTree as ET
 import xml.dom.minidom
-#from app.forms import BookQueryForm
+from app.forms import BookQueryForm
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import quote
 import re
 import math
 import random
-from Book import Book
+from app.Book import Book
 
 def available(book):
     # Important classes: 
@@ -52,6 +52,7 @@ def get_titles(user_id, extra_url, number_of_books):
     current_page = 1
     titles = []
     while current_page <= number_of_pages:
+        print(current_page)
         URL = URL.replace("&page="+str(current_page-1), "&page="+str(current_page))
         page = requests.get(URL)
         soup = BeautifulSoup(page.content, 'html.parser')
@@ -63,56 +64,32 @@ def get_titles(user_id, extra_url, number_of_books):
             author = " ".join(book.find(class_ = "field author").find("a").text.split(",")[::-1]).strip()
             page_count = page_count if (page_count := (book.find(class_="field num_pages").find(class_="value").text.replace("pp", "").replace(",", "").strip())).isdigit() else 0
             rating = float(book.find(class_="field avg_rating").find(class_="value").text.strip())
-            goodreads_link = "goodreads.com" + book.find("a")["href"]
+            goodreads_link = "https://goodreads.com" + book.find("a")["href"]
             image_link = book.find("img")["src"].replace("_SX50_", "").replace("_SY75_", "").replace("..", ".")
             titles.append(Book(title, author, page_count, rating, goodreads_link, image_link))
         current_page += 1
     return titles
 
-#@app.route('/', methods=["GET", "POST"])
-#@app.route('/index', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
+@app.route('/index', methods=["GET", "POST"])
 def index():
-    # form = BookQueryForm()
-    # shelves = get_shelves("64346486")
-    # form.shelf.choices = shelves.keys()
-
-    # if form.validate_on_submit():
-    #    #print(r.content)
-    #    count = 0
-    #    i = 0
-    #    titles = get_titles("64346486", "/review/list/64346486-s-rrenn-gwyire?shelf=to-read&print=true", 463)
-    #    available_books = []
-    #    print("got titles")
-    #    while i < len(titles) and count < int(form.number_of_books.data):
-    #        if available(titles[i]):
-    #            available_books.append(titles[i])
-    #            count += 1
-    #            print("one available", i, count)
-    #        i += 1
-    #    return render_template("results.html", books=available_books)
-    # return render_template("index.html", form=form)
-    # user_id = input("Please enter your Goodreads id: ")
-    # shelves = get_shelves(user_id)
-    # print("Please enter one of the following options:")
-    # for shelf in shelves.keys():
-    #    print(shelf)
-    # shelf_choice = input("Enter your selection here: ").lower()
-    # if shelf_choice not in shelves:
-    #    print("This is not one of your shelves. Bye.")
-    #    return
-    titles = get_titles("64346486", "/review/list/64346486-s-rrenn-gwyire?shelf=to-read&print=true", 463)
-    # titles = get_titles(user_id, shelves[shelf_choice][0], shelves[shelf_choice][1])
-    random.shuffle(titles)
-    i = 0
-    find_num = 5
-    count = 0
-    available_books = []
-    while i < len(titles) and count < find_num:
-        if available(titles[i]):
-            available_books.append(titles[i])
-            count += 1
-            print(titles[i].title)
-        i += 1
-
-index()
+    form = BookQueryForm()
+    shelves = get_shelves("64346486")
+    form.shelf.choices = list(shelves.keys())
+    if form.validate_on_submit():
+       #print(r.content)
+       count = 0
+       i = 0
+       titles = get_titles("64346486", shelves[form.shelf.data][0], shelves[form.shelf.data][1])
+       random.shuffle(titles)
+       available_books = []
+       print("got titles")
+       while i < len(titles) and count < int(form.number_of_books.data):
+           if available(titles[i]):
+               available_books.append(titles[i])
+               count += 1
+               print("one available", i, count)
+           i += 1
+       return render_template("results.html", books=available_books)
+    return render_template("index.html", form=form)
     
