f3b1ba9d6b38354b876e9c30b28d311b6b7e669c
Oren Scheer <oren.rs723@gmail.com>
2021-08-21 20:13:06 -0400

    Remove legacy Flask app

--- legacy/app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, RadioField, IntegerField
-from wtforms.validators import DataRequired, NumberRange
-
-class BookQueryForm(FlaskForm):
-    shelf = RadioField("Shelf", coerce=str, validators=[DataRequired()])
-    number_of_books = IntegerField("Number of results", validators=[NumberRange(min=1, max=10, message="Number of books must be within 1 and 10."), DataRequired()])
-    submit = SubmitField("Find my NextAvailableRead(s)")
